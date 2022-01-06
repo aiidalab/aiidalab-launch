370cdbd0c3b7992333976591e3b6ddd959836602
Carl Simon Adorf <simon.adorf@epfl.ch>
2022-01-06 11:52:12 +0100

    CI: Use mypy to check type annotations. (#34)
    * CI: Use mypy to check type annotations.
    
    * Extend typing in util and core modules.

--- aiidalab_launch/core.py
+++ aiidalab_launch/core.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# __future__ import needed for classmethod factory functions; should be dropped
+# with py 3.10.
+from __future__ import annotations
+
 import asyncio
 import logging
 import time
@@ -8,7 +12,7 @@ from dataclasses import asdict, dataclass, field
 from enum import Enum, auto
 from pathlib import Path
 from secrets import token_hex
-from typing import List, Optional
+from typing import Any, AsyncGenerator, Generator, List, Optional, Union
 from uuid import uuid4
 
 import docker
@@ -27,11 +31,11 @@ APPLICATION_ID = "org.aiidalab.aiidalab_launch"
 LOGGER = logging.getLogger(APPLICATION_ID.split(".")[-1])
 
 
-def _default_home_mount():
+def _default_home_mount() -> str:
     return str(Path.home().joinpath("aiidalab"))
 
 
-def _default_port():  # explicit function required to enable test patching
+def _default_port() -> int:  # explicit function required to enable test patching
     return DEFAULT_PORT
 
 
@@ -44,21 +48,21 @@ class Profile:
     image: str = "aiidalab/aiidalab-docker-stack:latest"
     home_mount: Optional[str] = field(default_factory=lambda: _default_home_mount())
 
-    def container_name(self):
+    def container_name(self) -> str:
         return f"{CONTAINER_PREFIX}{self.name}"
 
-    def environment(self, jupyter_token):
+    def environment(self, jupyter_token: str) -> dict:
         return {
             "AIIDALAB_DEFAULT_APPS": " ".join(self.default_apps),
             "JUPYTER_TOKEN": str(jupyter_token),
             "SYSTEM_USER": self.system_user,
         }
 
-    def dumps(self):
+    def dumps(self) -> str:
         return toml.dumps({k: v for k, v in asdict(self).items() if k != "name"})
 
     @classmethod
-    def loads(cls, name, s):
+    def loads(cls, name: str, s: str) -> Profile:
         return cls(name=name, **toml.loads(s))
 
 
@@ -68,7 +72,7 @@ class Config:
     default_profile: str = MAIN_PROFILE_NAME
 
     @classmethod
-    def loads(cls, blob):
+    def loads(cls, blob: str) -> Config:
         config = toml.loads(blob)
         config["profiles"] = [
             Profile(name=name, **profile)
@@ -76,7 +80,7 @@ class Config:
         ]
         return cls(**config)
 
-    def dumps(self):
+    def dumps(self) -> str:
         config = asdict(self)
         config["profiles"] = {
             profile.pop("name"): profile for profile in config.pop("profiles", [])
@@ -84,10 +88,10 @@ class Config:
         return toml.dumps(config)
 
     @classmethod
-    def load(cls, path):
+    def load(cls, path: Path) -> Config:
         return cls.loads(path.read_text())
 
-    def save(self, path, safe=True):
+    def save(self, path: Path, safe: bool = True) -> None:
         path.parent.mkdir(exist_ok=True, parents=True)
         if safe:
             path_tmp = path.with_suffix(f".{uuid4()!s}")
@@ -96,7 +100,7 @@ class Config:
         else:
             path.write_text(self.dumps())
 
-    def get_profile(self, name):
+    def get_profile(self, name: str) -> Profile:
         for profile in self.profiles:
             if profile.name == name:
                 return profile
@@ -108,7 +112,9 @@ class FailedToWaitForServices(RuntimeError):
 
 
 @contextmanager
-def _async_logs(container):
+def _async_logs(
+    container: docker.models.containers.Container,
+) -> Generator[AsyncGenerator[Any, None], None, None]:
     logs = container.logs(stream=True, follow=False)
     try:
         yield _async_wrap_iter(logs)
@@ -131,19 +137,19 @@ class AiidaLabInstance:
     _image: docker.models.images.Image = None
     _container: docker.models.containers.Container = None
 
-    def _get_image(self):
+    def _get_image(self) -> Optional[docker.models.images.Image]:
         try:
             return self.client.images.get(self.profile.image)
         except docker.errors.ImageNotFound:
             return None
 
-    def _get_container(self):
+    def _get_container(self) -> Optional[docker.models.containers.Container]:
         try:
             return self.client.containers.get(self.profile.container_name())
         except docker.errors.NotFound:
             return None
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self._image = self._get_image()
         self._container = self._get_container()
 
@@ -155,8 +161,12 @@ class AiidaLabInstance:
     def container(self) -> Optional[docker.models.containers.Container]:
         return self._container
 
+    def _requires_container(self) -> None:
+        if self.container is None:
+            raise RuntimeError("This function requires a container instance.")
+
     @property
-    def _mounts(self):
+    def _mounts(self) -> List[docker.types.Mount]:
         return (
             [
                 docker.types.Mount(
@@ -169,7 +179,7 @@ class AiidaLabInstance:
             else []
         )
 
-    def pull(self):
+    def pull(self) -> docker.models.images.Image:
         try:
             image = self.client.images.pull(self.profile.image)
             LOGGER.info(f"Pulled image: {image}")
@@ -193,33 +203,42 @@ class AiidaLabInstance:
         )
         return self._container
 
-    def start(self):
+    def start(self) -> None:
         LOGGER.info(f"Starting container '{self.profile.container_name()}'...")
         (self.container or self.create()).start()
+        assert self.container is not None
         LOGGER.info(f"Started container: {self.container.name} ({self.container.id}).")
 
-    def restart(self):
+    def restart(self) -> None:
+        self._requires_container()
+        assert self.container is not None
         self.container.restart()
 
-    def stop(self, timeout=None):
+    def stop(self, timeout: Optional[float] = None) -> None:
+        self._requires_container()
+        assert self.container is not None
         try:
             self.container.stop(timeout=timeout)
         except AttributeError:
             raise RuntimeError("no container")
 
-    def remove(self):
+    def remove(self) -> None:
+        self._requires_container()
+        assert self.container is not None
         try:
             self.container.remove()
             self._container = None
         except AttributeError:
             raise RuntimeError("no container")
 
-    def logs(self, stream=False, follow=False):
+    def logs(
+        self, stream: bool = False, follow: bool = False
+    ) -> Union[docker.types.daemon.CancellableStream, str]:
         if self.container is None:
             raise RuntimeError("Instance was not created.")
         return self.container.logs(stream=stream, follow=follow)
 
-    def exec_create(self, cmd, privileged=False):
+    def exec_create(self, cmd: str, privileged: bool = False) -> str:
         LOGGER.info(f"Executing: {' '.join(cmd)}")
         if self.container is None:
             raise RuntimeError("Instance was not created.")
@@ -231,7 +250,7 @@ class AiidaLabInstance:
             workdir=None if privileged else f"/home/{self.profile.system_user}",
         )["Id"]
 
-    async def _wait_for_services(self):
+    async def _wait_for_services(self) -> None:
         if self.container is None:
             raise RuntimeError("Instance was not created.")
 
@@ -242,7 +261,8 @@ class AiidaLabInstance:
             None, self.container.exec_run, "wait-for-services"
         )
 
-        async def _echo_logs():
+        async def _echo_logs() -> None:
+            assert self.container is not None
             with _async_logs(self.container) as logs:
                 async for chunk in logs:
                     if logging.DEBUG < LOGGER.getEffectiveLevel() < logging.ERROR:
@@ -264,7 +284,7 @@ class AiidaLabInstance:
         else:
             LOGGER.info(f"Services are up ({self.container.id}).")
 
-    def wait_for_services(self, timeout=None):
+    def wait_for_services(self, timeout: Optional[float] = None) -> None:
         start = time.time()
         try:
             asyncio.run(asyncio.wait_for(self._wait_for_services(), timeout))
@@ -277,7 +297,7 @@ class AiidaLabInstance:
             # likely fail.
             time.sleep(5)
 
-    def status(self, timeout=3) -> AiidaLabInstanceStatus:
+    def status(self, timeout: Optional[float] = 3.0) -> AiidaLabInstanceStatus:
         if self.container:
             self.container.reload()
             if self.container.status == "running":
@@ -300,15 +320,15 @@ class AiidaLabInstance:
             result = self.container.exec_run("/bin/sh -c 'echo $JUPYTER_TOKEN'")
             if result.exit_code == 0:
                 return result.output.decode().strip()
-            else:
-                return None
+        return None
 
     def host_port(self) -> Optional[int]:
         if self.container:
             try:
                 return self.container.ports["8888/tcp"][0]["HostPort"]
             except (KeyError, IndexError):
-                return None
+                pass
+        return None
 
     def url(self) -> str:
         if self.status() is not self.AiidaLabInstanceStatus.UP:
