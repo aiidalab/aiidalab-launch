aiidalab/aiidalab-launch

commit d848856207cf08aaa4e6e4547dafea4ca959056c

Feature/auto recreate on image mismatch2 (#33)
* Automatically recreate container if image has changed.

* Update tests.

* Add image property to Instance.

* Refactor the start logic.

* Provide better indication about pull wait times.

Based on whether the image is pulled for the first time or not.

aiidalab_launch/__main__.py
@@ -307,35 +307,62 @@ def start(app_state, profile, restart, wait, pull, no_browser, show_ssh_help, fo
                 abort=True,
             )
 
+    # Obtain image (either via pull or local).
+    if pull:
+        try:
+            msg = (
+                f"Downloading image '{instance.profile.image}', this may take a while..."
+                if instance.image is None
+                else f"Downloading latest version of '{instance.profile.image}'..."
+            )
+            with spinner(msg):
+                instance.pull()
+        except RuntimeError as error:
+            raise click.ClickException(str(error))
+    elif instance.image is None:
+        raise click.ClickException(
+            f"Unable to find image '{profile.image}'. "
+            "Try to use '--pull' to pull the image prior to start."
+        )
+
+    # Check if image has changed.
+    assert instance.image is not None
+    recreate = instance.container and instance.container.image.id != instance.image.id
+
     try:
-        if instance.container is None:
-            if pull:
-                with spinner(
-                    f"Downloading image '{instance.profile.image}' (this may take a while)..."
-                ):
-                    instance.pull()
-            else:
-                raise click.ClickException(
-                    f"Unable to find container '{instance.profile.container_name()}'. "
-                    "Use '--pull' to pull the image prior to start."
-                )
 
         InstanceStatus = instance.AiidaLabInstanceStatus  # local alias for brevity
 
         status = instance.status()
-        if status is InstanceStatus.DOWN:
+        if status in (
+            InstanceStatus.DOWN,
+            InstanceStatus.CREATED,
+            InstanceStatus.EXITED,
+        ):
+            if recreate:
+                with spinner("Recreating container..."):
+                    instance.remove()
+                    instance.create()
             with spinner("Starting container..."):
                 instance.start()
-        elif status is InstanceStatus.CREATED:
-            with spinner("Starting previously created container..."):
-                instance.restart()
         elif status is InstanceStatus.UP and restart:
             with spinner("Restarting container..."):
-                instance.restart()
+                if recreate:
+                    instance.stop()
+                    instance.remove()
+                instance.start()
         elif status is InstanceStatus.UP and not restart:
-            click.echo(
-                "Container was already running, use --restart to restart it.", err=True
-            )
+            if recreate:
+                click.secho(
+                    "Container is already running, however the image has changed. "
+                    "A restart with --restart is recommended.",
+                    fg="yellow",
+                )
+            else:
+                click.echo(
+                    "Container was already running, use --restart to restart it.",
+                    err=True,
+                )
         elif status is InstanceStatus.STARTING:
             click.echo("Container is already starting up...", err=True)
 

aiidalab_launch/core.py
@@ -123,11 +123,19 @@ class AiidaLabInstance:
         CREATED = auto()
         DOWN = auto()
         UP = auto()
+        EXITED = auto()
         STARTING = auto()
 
     client: docker.DockerClient
     profile: Profile
+    _image: docker.models.images.Image = None
     _container: docker.models.containers.Container = None
+
+    def _get_image(self):
+        try:
+            return self.client.images.get(self.profile.image)
+        except docker.errors.ImageNotFound:
+            return None
 
     def _get_container(self):
         try:
@@ -136,7 +144,12 @@ class AiidaLabInstance:
             return None
 
     def __post_init__(self):
+        self._image = self._get_image()
         self._container = self._get_container()
+
+    @property
+    def image(self) -> Optional[docker.models.images.Image]:
+        return self._image
 
     @property
     def container(self) -> Optional[docker.models.containers.Container]:
@@ -160,6 +173,7 @@ class AiidaLabInstance:
         try:
             image = self.client.images.pull(self.profile.image)
             LOGGER.info(f"Pulled image: {image}")
+            self._image = image
             return image
         except docker.errors.ImageNotFound:
             raise RuntimeError(f"Unable to pull image: {self.profile.image}")
@@ -169,14 +183,9 @@ class AiidaLabInstance:
             LOGGER.info(f"Ensure home mount point ({self.profile.home_mount}) exists.")
             Path(self.profile.home_mount).mkdir(exist_ok=True)
 
-        try:
-            image = self.client.images.get(self.profile.image)
-        except docker.errors.ImageNotFound:
-            image = self.pull()
-
         assert self._container is None
         self._container = self.client.containers.create(
-            image=image,
+            image=(self.image or self.pull()),
             name=self.profile.container_name(),
             environment=self.profile.environment(jupyter_token=token_hex(32)),
             mounts=self._mounts,
@@ -282,6 +291,8 @@ class AiidaLabInstance:
                     return self.AiidaLabInstanceStatus.UP
             elif self.container.status == "created":
                 return self.AiidaLabInstanceStatus.CREATED
+            elif self.container and self.container.status == "exited":
+                return self.AiidaLabInstanceStatus.EXITED
         return self.AiidaLabInstanceStatus.DOWN
 
     def jupyter_token(self) -> Optional[str]:

tests/test_core.py
@@ -80,6 +80,7 @@ def test_instance_create_remove(instance):
 @pytest.mark.slow
 @pytest.mark.trylast
 def test_instance_start_stop(instance):
+    assert instance.status() is instance.AiidaLabInstanceStatus.DOWN
     instance.start()
     sleep(0.1)
     assert instance.status() is instance.AiidaLabInstanceStatus.STARTING
@@ -90,4 +91,6 @@ def test_instance_start_stop(instance):
     instance.wait_for_services(timeout=300)
     assert instance.status() is instance.AiidaLabInstanceStatus.UP
     instance.stop()
+    assert instance.status() is instance.AiidaLabInstanceStatus.EXITED
+    instance.remove()
     assert instance.status() is instance.AiidaLabInstanceStatus.DOWN

commit 040f3b44d50b9eefed8c495efd91f7205b9734d2

Fix: Actually restart the container with start --restart.
Regression was introduced in
https://github.com/aiidalab/aiidalab-launch/pull/33 and released with
https://github.com/aiidalab/aiidalab-launch/releases/tag/v2022.1007 .

Fixes #51.

aiidalab_launch/__main__.py
@@ -360,7 +360,9 @@ def start(app_state, profile, restart, wait, pull, no_browser, show_ssh_help, fo
                 if recreate:
                     instance.stop()
                     instance.remove()
-                instance.start()
+                    instance.start()
+                else:
+                    instance.restart()
         elif status is InstanceStatus.UP and not restart:
             if recreate:
                 click.secho(