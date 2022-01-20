8aff5f2ca91835c86487a27c64d3205d6ce5387e
Simon Adorf <carl.simon.adorf@gmail.com>
2022-01-20 18:18:18 +0100

    fixup! Refactor status to run asynchronously.

--- aiidalab_launch/core.py
+++ aiidalab_launch/core.py
@@ -381,7 +381,7 @@ class AiidaLabInstance:
             if self.container.status == "running":
                 try:
                     await asyncio.wait_for(self._wait_for_services(), timeout=timeout)
-                except TimeoutError:
+                except asyncio.exceptions.TimeoutError:
                     return self.AiidaLabInstanceStatus.STARTING
                 except RuntimeError:
                     return self.AiidaLabInstanceStatus.UNKNOWN
