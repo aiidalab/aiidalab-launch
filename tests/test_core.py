b5a72256ccc9c07c728c2e86a9b1050c073190d6
Simon Adorf <carl.simon.adorf@gmail.com>
2022-02-08 09:17:26 +0100

    Tests: Implement unit test for config save.

--- tests/test_core.py
+++ tests/test_core.py
@@ -62,6 +62,12 @@ def test_config_dumps_loads(config):
     assert config == Config.loads(config.dumps())
 
 
+@pytest.mark.parametrize("safe", [True, False])
+def test_config_save(tmp_path, config, safe):
+    config.save(tmp_path / "config.json", safe=safe)
+    assert Config.load(tmp_path / "config.json") == config
+
+
 def test_config_version(config):
     assert config.version is None
 
