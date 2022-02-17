c14a3fb2e7426ec2ec72e797e0539066f31243aa
Simon Adorf <carl.simon.adorf@gmail.com>
2022-02-17 11:34:58 +0100

    Tests: Add test for migration with existing container.

--- tests/test_application_state.py
+++ tests/test_application_state.py
@@ -30,3 +30,18 @@ def test_apply_migrations_aiidalab_dir_exists(application_state):
     assert aiidalab_dir.samefile(
         Path(config.get_profile(config.default_profile).home_mount)
     )
+
+
+def test_apply_migrations_aiidalab_container_exists(application_state, instance):
+    aiidalab_dir = Path.home().joinpath("aiidalab")
+    assert not application_state.config_path.is_file()
+    assert not aiidalab_dir.exists()
+    aiidalab_dir.mkdir()
+    assert aiidalab_dir.is_dir()
+    container = instance.create()
+    application_state.apply_migrations()
+    assert application_state.config_path.is_file()
+    config = application_state.config
+    default_profile = config.get_profile(config.default_profile)
+    assert not Path(default_profile.home_mount).is_absolute()
+    assert default_profile == type(default_profile).from_container(container)
